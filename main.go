220175235446ad69184a938c2fee9788015fa227
Tushar Sharma <tushar123cse@gmail.com>
2019-11-03 15:27:35 +0530

    added logic to get data through end points

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -9,26 +10,39 @@ import (
 
 //Book struct(Model)
 type Book struct {
-	ID     string `json:"id"`
-	Isbn   string `json:"isbn"`
-	Title  string `json:"title"`
-	Author Author `json:"author"`
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
-//type Author struct
-
+//Author type struct
 type Author struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 
+//Init books var as a 'slice' Book struct
+var books []Book
+
 //Get All books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 //Get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r) //Get params
+	//Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 
 }
 
@@ -48,6 +62,13 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 func main() {
 	//Init router
 	r := mux.NewRouter()
+
+	//mock Data
+	// TODO : Add to a database
+
+	books = append(books, Book{ID: "1", Isbn: "432124", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+
+	books = append(books, Book{ID: "2", Isbn: "343434", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
 	//Route Handlers/Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
